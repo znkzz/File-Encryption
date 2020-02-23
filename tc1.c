9d9e0fa9f2a8a068e9a7938e5177f294c7cba2d7
vzpul <42790878+znkzz@users.noreply.github.com>
2020-02-23 13:34:24 +0800

    v1.4
    1.提高验证的准确性
    2.-del 提供加密/解密后删除文件
    3. 文本补充
    4.修复bug
    5.不兼容v1.3了

--- tc1.c
+++ tc1.c
@@ -15,7 +15,7 @@ v1.4 TODO
 1.提高验证的准确性 
 2.-del 提供加密/解密后删除文件
 3. 文本补充 
-
+4.修复bug 
 v1.5 TODO
 
 1.-mess 提供双重加密，提高安全性 
@@ -23,17 +23,17 @@ v1.5 TODO
 
 */
 int  main(int argc,char*argv[]) {
-		notice("文件加密器 版本:v1.3 date:2020\\2\\15 by znkzz");
-	if(argc!=5) {
+		notice("文件加密器 版本:v1.4 date:2020\\2\\19 by znkzz");
+	if(argc<5) {
 		error("命令行参数错误!");
 		exit(-1);
 	}
 	Key=argv[2];
-
+short code;
 	if(strcmp(argv[1],"-en")==0) {
-		short code=	EncodeFile(argv[3],argv[4])	;
+		code=EncodeFile(argv[3],argv[4])	;
 		if(code==1) {
-			notice("文件为空!");
+			notice("文件为空或已有指定的加密后文件!");
 		} else if (code==-1) {
 			error("无法打开文件!");
 		} else if (code==-2) {
@@ -44,9 +44,9 @@ int  main(int argc,char*argv[]) {
 			successful("加密完成.");
 		}
 	} else if(strcmp(argv[1],"-de")==0) {
-		short code=	DecodeFile(argv[3],argv[4]) ;
+		code=DecodeFile(argv[3],argv[4]) ;
 		if(code==1) {
-			notice("文件为空!");
+			notice("文件为空或已有指定的解密后的文件!");
 		} else if (code==-1) {
 			error("无法打开文件!");
 		} else if (code==-2) {
@@ -57,13 +57,21 @@ int  main(int argc,char*argv[]) {
 			successful("解密完成.");
 		} else if (code==-4) {
 			error("文件的密钥不正确.");
+		} else if (code==-5) {
+			error("不支持解密v1.3的文件,请使用本软件的1.3版本进行解密.");
 		}
-		return 0;
+	
 	} else {
 		error("命令行参数非法!");
 		return -1;
 	}
-
+if (argc==6&&strcmp(argv[5],"-del")==0&&code==0) {
+	remove(argv[3]);
+}else if(argc==6&&strcmp(argv[5],"-del")==0&&code!=0)
+{
+	error("解密或加密过程中出错,因此不能删除原文件");
+}
+return 0;
 
 //	DecodeFile(argv[3],argv[4]);
 //	getchar();
@@ -83,17 +91,20 @@ short EncodeFile(const  char * File_0,const char *File_E/*,char (*p) (void)*/) {
 		return 1;
 	}
 	if(StrLong>512*1024*1024) {
+		remove(File_E);
 		fclose(f1);
 		fclose(f2);
 		return 2;
 	}
 
 	if(!StrLong||!f1||!f2) {
+		remove(File_E);
 		return -1;
 	}
 	char *pDATA = (char *)malloc(StrLong*sizeof(char));
 
 	if(pDATA==NULL) {
+		remove(File_E);
 		return -2;
 	}
 	successful("文件内容已成功读取.");
@@ -109,6 +120,9 @@ short EncodeFile(const  char * File_0,const char *File_E/*,char (*p) (void)*/) {
 	successful("文件已加密完毕.正在写文件中······");
 	fwrite(pDATA, sizeof(char), StrLong, f2);
 	fputc(10^(int)GetRanDomKey(),f2);
+		fputc(11^(int)GetRanDomKey(),f2);
+		fputc(12^(int)GetRanDomKey(),f2);
+		fputc(13^(int)GetRanDomKey(),f2);
 	free(pDATA);
 	fclose(f1);
 	fclose(f2);
@@ -131,6 +145,8 @@ short DecodeFile(const  char * File_0,const char *File_E/*,char (*p) (void)*/) {
 
 	if(StrLong==0) {
 
+fclose(f1);//bug fix 2.19 
+		fclose(f2);
 		return 1;
 	}
 	if(StrLong>512*1024*1024) {
@@ -155,22 +171,28 @@ short DecodeFile(const  char * File_0,const char *File_E/*,char (*p) (void)*/) {
 	for (i = 0; i < StrLong; i++) {
 
 		pDATA[i]^=(GetRanDomKey());
-printf("%d\t",pDATA[i]);
+// printf("%d\t",pDATA[i]);
 	}
 	successful("文件已解密完毕.正在效验文件中······");
 
 	short code;
-	if((int)(pDATA[i-1])==10) {
-		fwrite(pDATA, sizeof(char), StrLong-1, f2);
+	if((int)(pDATA[i-4])==10&&(int)(pDATA[i-3])==11&&(int)(pDATA[i-2])==12&&(int)(pDATA[i-1])==13) {
+		fwrite(pDATA, sizeof(char), StrLong-4, f2);
 		code=0;
 		
 	} else {
+		if((int)(pDATA[i-1])==10) {
+	    remove(File_E);
+		code=-5;
+		
+	} else { 
 		error("文件效验失败!错误信息:") ;
 		printf("[%c * %d]\n",pDATA[i],(int)(pDATA[i]));
 		printf("[%c * %d]\n",pDATA[i+1],(int)(pDATA[i+1]));
 		printf("[%c * %d]\n",pDATA[i-1],(int)(pDATA[i-1]));
 		remove(File_E);
 		code=-4;
+		} 
 	}
 
 	free(pDATA);
